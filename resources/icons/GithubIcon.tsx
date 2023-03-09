cf35757ee0ec11ebc72fd9cc120612f6cf0d498b
Theerapat Muangpoon <chansk131@gmail.com>
2023-03-10 03:39:14 +0700

    Fix svg style

--- resources/icons/GithubIcon.tsx
+++ resources/icons/GithubIcon.tsx
@@ -7,7 +7,7 @@ const GithubIcon = (props: Props) => {
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       role="img"
       viewBox="0 0 24 24"
       height="1em"
